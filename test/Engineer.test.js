6746cf6b954291a8487b5657de5dc54c7f01642f
ibraim martinez <ibraimm5@gmail.com>
2021-08-14 13:57:29 -0400

    adding

---
+++ test/Engineer.test.js
@@ -0,0 +1,9 @@
+const Engineer = require("../engineer"); 
+ 
+ describe("Engineer", () => {
+    it("write a profile for the engineer", () => {
+        const pro = new Engineer(); 
+
+        expect("value" in obj).toEqual(true);
+    });
+ })
